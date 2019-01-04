20c6e604ee39d18425f24a718b648ef2259f2a11
saru <sarumathiayyappan@gmail.com>
2019-01-04 12:11:25 +0530

    Update and rename amstronginterval.python2 to amstronginterval.c

---
+++ amstronginterval.c
@@ -0,0 +1,23 @@
+# amstrong
+#include<stdio.h>
+#include<conio.h>
+int ams
+{
+int a,b,s,temp,digit,sum,n;
+printf("enter the first value");
+printf("enter the second value");
+ for n in range(a,b)
+   sum = 0;
+   temp = s;
+   while temp > 0
+       digit = temp % 10;
+       sum =sum+ digit ** 3;
+       temp /= 10;
+ 
+   if s == sum
+   {
+       printf("the interval value is",s);
+   }
+ }
+   return 0;
+     
